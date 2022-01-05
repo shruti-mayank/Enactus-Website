2315651c4ae703630e9fff433f2cab8c705b1ba2
HimanshT <himanshutripathi4462@gmail.com>
2022-01-05 12:30:37 +0530

    testing branch

--- enactus_website_2021/app.js
+++ enactus_website_2021/app.js
@@ -2,10 +2,35 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const database = require('./db');
+const path = require('path');//for connecting views
+const Member = require('./models/member');
+const Project = require('./models/project');
 
-require('./models/member');
-require('./models/project');
+app.set('view engine', 'ejs');//for connecting views
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT,()=>{
+app.get('/newproject', (req, res) => {
+    res.render()
+})
+app.post('/newproject', async (req, res) => {
+    const project = new Project(req.body.project);
+    await project.save();
+    console.log(project);
+    res.redirect();
+})
+
+app.get('/newmember', (req, res) => {
+    res.render()
+})
+app.post('/newmember', async (req, res) => {
+    const member = new Member(req.body.member);
+    await member.save();
+    console.log(member);
+    res.redirect();
+})
+
+
+app.listen(process.env.PORT, () => {
     console.log("server started on port :" + process.env.PORT);
 });
